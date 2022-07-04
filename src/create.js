4731322ef29bf8752c8c1e75147bd2176c6af707
doctype-Melvin <doctype.melvin@gmail.com>
2022-07-04 11:46:20 +0200

    adds grid to display section and appends todo cards

--- src/create.js
+++ src/create.js
@@ -1,8 +1,10 @@
 //////DOM Start
+const display = document.querySelector('.display'); //Section where tasks are rendered
+const controls = document.querySelector('.controls');
 const main = document.querySelector('.main')
 const button = document.createElement('button')
 button.textContent = 'Button'
-main.append(button);
+controls.append(button);
 
 const taskModal = document.querySelector('.taskModal');
 const openForm = () => {
@@ -31,12 +33,44 @@ const makeTask = () => { //Creates object from form inpt values
     return newTask
 }
 
+//Creates the dom nodes for task cards
+const createCard = ([task, note, date]) => {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    let title = document.createElement('span');
+    title.classList.add('cardTitle');
+    title.textContent = task;
+    let description = document.createElement('span');
+    description.classList.add('cardNote');
+    description.textContent = note;
+    let due = document.createElement('span');
+    due.classList.add('dateNote');
+    due.textContent = date;
+    
+    card.append(title, description, due)
+    return card
+}
+
 const addTaskBtn = document.querySelector('.newTaskBtn');
 addTaskBtn.onclick = (e) => {
     e.preventDefault();
-    addToStorage()
+    addToStorage();
+    readStorage();
     taskModal.style.display = 'none'
 }
+
+const readStorage = () => {
+    const tasksArray = JSON.parse(localStorage.getItem('tasks'))
+    if (tasksArray != null){
+        while (display.firstChild) { //remove all appended tasks
+            display.removeChild(display.lastChild)
+        }
+        tasksArray.map(item => display.append(createCard(Object.values(item))))
+    }
+}
+readStorage()
+//tasksArray.map(item => display.append(createCard(Object.values(item))))
+
 //////DOM End
 
 //Fn updates local storage tasks array
