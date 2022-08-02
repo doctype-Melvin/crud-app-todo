b50a8442254228c4a6de430778338f98a684c079
doctype-Melvin <doctype.melvin@gmail.com>
2022-08-02 11:07:15 +0200

    adds hoizontally scrollable navbar and hover styling

--- src/interaction.js
+++ src/interaction.js
@@ -1,14 +1,3 @@
-import icon from './burger-icon.png'
-
-const sidebar = document.querySelector('.sidebar')
-const buttons = document.querySelector('.buttons')
-const burger = new Image()
-burger.src = icon
-burger.classList.add('burger')
-sidebar.insertBefore(burger, buttons)
-
-burger.addEventListener('click', () => toggleDisplay(buttons))
-
 function toggleDisplay (element) {
   (element.style.display === 'none')
     ? element.style.display = 'block'
